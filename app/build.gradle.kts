53cb0b1a1dcbc3c91d9f8f1e450d18edae0a1a90
Erik Vaněk <erikvanek0@gmail.com>
2026-07-26 09:58:58 +0200

    Bumped version to 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.neddy.ketch"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "2.1"
+        versionCode = 7
+        versionName = "2.2"
 
         buildConfigField("String", "MAPS_API_KEY", "\"$mapsApiKey\"")
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
